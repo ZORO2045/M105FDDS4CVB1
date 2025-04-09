c5d5d980e2e60211a526f51909e6576d0a8385d3
ፚ Ꭷ Ꮢ Ꭷ ❥ | 𝘽𝙇𝘼𝘾𝙆_𝙎𝙃𝘼𝙍𝙆 <101139382+ZORO2045@users.noreply.github.com>
2025-04-09 13:58:33 +0200

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,7 @@ export PLATFORM_VERSION=10
 export ANDROID_MAJOR_VERSION=q
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
+export CROSS_COMPILE=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
 make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 m10lte_swa_open_defconfig
-make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 -j16
+make CONFIG_SECTION_MISMATCH_WARN_ONLY=y ARCH=arm64 -j$(nproc)
